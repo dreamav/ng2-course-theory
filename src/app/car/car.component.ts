fb6075e82c60add0961189df271d196055089c71
Mevericks <reloved@gmail.com>
2018-12-12 20:58:31 +0200

    селектор через атрибут тега

--- src/app/car/car.component.ts
+++ src/app/car/car.component.ts
@@ -1,18 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-car',
-  // templateUrl: './car.component.html',
-    template:`
-        <h2>Car here</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam architecto deserunt dolor eaque eveniet harum iure laboriosam magni modi neque non nostrum nulla quas quasi, quos saepe sit sunt voluptatum.</p>
-    `,
-  // styleUrls: ['./car.component.css']
-    styles:[`
-        h2{
-            color: brown;
-        }
-    `]
+  selector: '[app-car]',
+  templateUrl: './car.component.html',
+  styleUrls: ['./car.component.css']
 })
 export class CarComponent implements OnInit {
 
