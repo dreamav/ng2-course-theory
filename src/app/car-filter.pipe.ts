d51db8b08db9252ca04f25f891c4e4d67291f4a4
Mevericks <reloved@gmail.com>
2018-12-26 15:37:12 +0200

    Пайпы. carFilter метод transform

--- src/app/car-filter.pipe.ts
+++ src/app/car-filter.pipe.ts
@@ -6,7 +6,11 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class CarFilterPipe implements PipeTransform {
 
   transform(carList, searchStr: string) {
+      if (carList.length === 0 || searchStr === '') {
+          return carList;
+      }
 
+      return carList.filter(car => car.name === searchStr);
   }
 
 }
