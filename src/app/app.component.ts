a9eb46c8ab59ccd29cc7b0a0a5ac2bc4537032ff
Mevericks <reloved@gmail.com>
2018-12-26 15:20:34 +0200

    Пайпы. создание фильтра - подготовка

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,23 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-    <div class="col-xs-8 col-xs-offset-2">
-        <h2>{{ num | appPow:3:'=' }}</h2>
-    </div>
+      <div class="col-xs-8 col-xs-offset-2">
+          <ul class="list-group">
+              <li
+                  class="list-group-item"
+                  *ngFor="let car of cars; let i = index"
+              ><b>{{ i + 1 }}</b> {{car.name}} </li>
+          </ul>
+      </div>
   `
 })
 export class AppComponent {
-    num = 2
+    cars = [
+        {name: 'Ford', descr: 'WFM 1'},
+        {name: 'Mazda', descr: 'WFM 2'},
+        {name: 'Bently', descr: 'WFM 3'},
+        {name: 'Audi', descr: 'WFM 4'},
+        {name: 'Mercedes', descr: 'WFM 5'},
+        {name: 'BWM', descr: 'WFM 6'}
+    ]
 }
