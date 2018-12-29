85abf67a7a112cd0ff15944023948190f89ec6e7
Mevericks <reloved@gmail.com>
2018-12-29 15:46:39 +0200

    Сервисы. подготовка. проект купить-вернуть машины

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import {PowPipe} from "./pow.pipe";
-import { CarFilterPipe } from './car-filter.pipe';
+import { CarComponent } from './car/car.component';
+import { CarAddComponent } from './car-add/car-add.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-      PowPipe,
-      CarFilterPipe
+    CarComponent,
+    CarAddComponent
   ],
   imports: [
     BrowserModule,
