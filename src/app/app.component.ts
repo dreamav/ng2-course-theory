d0e7bf2f5461da1d472efc9b5a526cfc25516e15
Mevericks <reloved@gmail.com>
2019-01-03 14:00:56 +0200

    Сервисы. инжектировал сервис CarsService в CarAddComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,7 @@ import {CarsService} from "./cars.service";
 
 @Component({
     selector: 'app-root',
-    templateUrl: './app.component.html',
-    providers: [CarsService]
+    templateUrl: './app.component.html'
 })
 
 export class AppComponent implements OnInit{
--- src/app/car-add/car-add.component.ts
+++ src/app/car-add/car-add.component.ts
@@ -1,18 +1,20 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import {CarsService} from "../cars.service";
 
 @Component({
-  selector: 'app-car-add',
-  templateUrl: './car-add.component.html',
-  styleUrls: ['./car-add.component.css']
+    selector: 'app-car-add',
+    templateUrl: './car-add.component.html',
+    styleUrls: ['./car-add.component.css']
 })
-export class CarAddComponent{
+export class CarAddComponent {
 
-  @Output() onCarAdd = new EventEmitter<String>();
-  carName = '';
+    carName = '';
 
-  addCar() {
-    this.onCarAdd.emit(this.carName);
-    this.carName = '';
-  }
+    constructor(private service:CarsService){}
+
+    addCar() {
+        this.service.addCar(this.carName);
+        this.carName = '';
+    }
 
 }
