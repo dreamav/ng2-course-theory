b31bf7f58e485ca50e950b1c63afe0beac21b45b
Mevericks <reloved@gmail.com>
2019-01-13 13:34:03 +0200

    Формы. R добавил валидаторы для FormControl

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {FormControl, FormGroup} from "@angular/forms";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 
 @Component({
@@ -19,8 +19,8 @@ export class AppComponent implements OnInit{
 
     ngOnInit(){
         this.form = new FormGroup({
-            email: new FormControl(''),
-            pass: new FormControl(''),
+            email: new FormControl('',[Validators.required,Validators.required]),
+            pass: new FormControl('',Validators.required),
             country: new FormControl('ua'),
             answer: new FormControl('no'),
         })
