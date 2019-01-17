745a70f80b99b075c4bb616e3556683b7ed53ac6
Mevericks <reloved@gmail.com>
2019-01-17 15:17:37 +0200

    HTTP. передал хедеры с запросом get

--- src/app/cars.service.ts
+++ src/app/cars.service.ts
@@ -1,31 +1,40 @@
 import {Injectable} from "@angular/core";
-import {Http, Response} from "@angular/http";
+import {Http, Response, Headers} from "@angular/http";
 
 @Injectable()
-export class CarsService{
-    constructor (private http: Http){}
-
-    getCars(){
-       return this.http.get('http://localhost:3000/cars')
-           .map((response:Response)=>{
-               return response.json()
-           })
+export class CarsService {
+    constructor(private http: Http) {
     }
-    addCar(carName: string){
+
+    getCars() {
+        const headers = new Headers({
+            'Content-Type':'application/json; charset=utf8'
+        })
+        return this.http.get('http://localhost:3000/cars',{
+            headers: headers
+        })
+            .map((response: Response) => {
+                return response.json()
+            })
+    }
+
+    addCar(carName: string) {
         const data = {
             name: carName,
             color: 'blue'
         }
         return this.http.post('http://localhost:3000/cars', data)
-            .map((response:Response)=> response.json());
+            .map((response: Response) => response.json());
     }
-    changeColor(car:any,color:string){
+
+    changeColor(car: any, color: string) {
         car.color = color;
         return this.http.put(`http://localhost:3000/cars/${car.id}`, car)
-            .map((response:Response) => response.json());
+            .map((response: Response) => response.json());
     }
-    deleteCar(car:any){
+
+    deleteCar(car: any) {
         return this.http.delete(`http://localhost:3000/cars/${car.id}`)
-            .map((response:Response) => response.json());
+            .map((response: Response) => response.json());
     }
 }
