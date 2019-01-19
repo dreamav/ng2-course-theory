3b682d4e5749db4b4c0bc2809fe8907ed18b385f
Mevericks <reloved@gmail.com>
2019-01-19 14:09:39 +0200

    РОУТЫ. получение и вывод queryParams в компоненте

--- src/app/car-page/car-page.component.ts
+++ src/app/car-page/car-page.component.ts
@@ -10,12 +10,16 @@ export class CarPageComponent implements OnInit {
 
   id:number;
   name:string;
+  color:string;
+  year:string;
 
   constructor(private route:ActivatedRoute) { }
 
   ngOnInit() {
     this.id = +this.route.snapshot.params['id'];
     this.name = this.route.snapshot.params['name'];
+    this.color = this.route.snapshot.queryParams['color'];
+    this.year = this.route.snapshot.queryParams['year'];
 
     this.route.params.subscribe((params:Params)=>{
       this.id = Number(params['id'])
