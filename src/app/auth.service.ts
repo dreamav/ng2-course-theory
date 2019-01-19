f0d0c4dcd9c8006143d6f63caa0755843ba86e39
Mevericks <reloved@gmail.com>
2019-01-19 19:56:48 +0200

    РОУТЫ. метод isAuth

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,5 +1,14 @@
 export class AuthService{
   isLoggedIn = false
+
+  isAuth(){
+    return new Promise((resolve,reject)=>{
+      setTimeout(()=>{
+        resolve(this.isLoggedIn)
+      }, 1000)
+    })
+  }
+
   logIn(){
     this.isLoggedIn = true
   }
