691b4579666e3f5e19e53928d1f6f08a71d0ef8d
Mevericks <reloved@gmail.com>
2019-01-19 18:29:56 +0200

    РОУТЫ. показать стр с ошибкой - notFoundComponent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import {CarsPageComponent} from "./cars-page/cars-page.component";
 import {RouterModule, Routes} from "@angular/router";
 import {HomePageComponent} from "./home-page/home-page.component";
 import {CarPageComponent} from "./car-page/car-page.component";
+import {NotFoundComponent} from "./not-found/not-found.component";
 
 const appRoutes: Routes = [
   {path: 'cars', component: CarsPageComponent, children:[
       {path: ':id/:name', component: CarPageComponent}
   ]},
-  {path: '', component: HomePageComponent}
+  {path: '', component: HomePageComponent},
+  {path: '**', component: NotFoundComponent}
 ]
 
 @NgModule({
