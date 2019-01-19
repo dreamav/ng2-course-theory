cf45aa56701a6d750e2bc5b29feae7053af779f9
Mevericks <reloved@gmail.com>
2019-01-19 20:13:11 +0200

    РОУТЫ. метод canActivate в auth-guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,9 +4,10 @@ import {RouterModule, Routes} from "@angular/router";
 import {HomePageComponent} from "./home-page/home-page.component";
 import {CarPageComponent} from "./car-page/car-page.component";
 import {NotFoundComponent} from "./not-found/not-found.component";
+import {AuthGuard} from "./auth-guard.service";
 
 const appRoutes: Routes = [
-  {path: 'cars', component: CarsPageComponent, children:[
+  {path: 'cars', component: CarsPageComponent,canActivate:[AuthGuard], children:[
       {path: ':id/:name', component: CarPageComponent}
   ]},
   {path: '', component: HomePageComponent},
