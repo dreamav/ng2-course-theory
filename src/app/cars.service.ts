157ec1b3e1f8d04c8a32aea70ff3132d93558ec8
Mevericks <reloved@gmail.com>
2019-01-19 13:56:21 +0200

    РОУТЫ. добавил свойства year color в массив cars

--- src/app/cars.service.ts
+++ src/app/cars.service.ts
@@ -5,19 +5,27 @@ export class CarsService {
   cars = [
     {
       name: 'Ford',
-      id: 1
+      id: 1,
+      year: 2017,
+      color: 'red'
     },
     {
       name: 'Audi',
-      id: 2
+      id: 2,
+      year: 2012,
+      color: 'black'
     },
     {
       name: 'BMW',
-      id: 3
+      id: 3,
+      year: 2010,
+      color: 'yellow'
     },
     {
       name: 'Mercedes',
-      id: 4
+      id: 4,
+      year: 2001,
+      color: 'green'
     }
   ]
 }
