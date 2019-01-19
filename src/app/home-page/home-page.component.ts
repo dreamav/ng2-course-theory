5abd9fbe0b0e1ee88cfac4119a0739685d6f9214
Mevericks <reloved@gmail.com>
2019-01-19 12:17:33 +0200

    РОУТЫ. программная навигация относительный путь через ActivatedRoute

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {Router} from "@angular/router";
+import {ActivatedRoute, Router} from "@angular/router";
 
 @Component({
   selector: 'app-home-page',
@@ -8,12 +8,12 @@ import {Router} from "@angular/router";
 })
 export class HomePageComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private route: ActivatedRoute) { }
 
   ngOnInit() {
   }
 
   openCarsPage(){
-    this.router.navigate(['/cars'])
+    this.router.navigate(['cars'], {relativeTo: this.route})
   }
 }
