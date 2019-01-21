84f62e5394eb1ccc500b15ae53d58db0862fb9ff
Mevericks <reloved@gmail.com>
2019-01-21 22:07:49 +0200

    АНИМАЦИИ. начальный state для div

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,23 @@
 import { Component } from '@angular/core';
+import {state, style, trigger} from "@angular/animations";
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
+  animations: [
+    trigger('clickedDiv', [
+      state('start', style({
+        backgroundColor: 'blue',
+        width: '150px',
+        height: '150px'
+      })),
+      state('end', style({
+        backgroundColor: 'blue',
+        width: '150px',
+        height: '150px'
+      }))
+    ])
+  ]
 })
 export class AppComponent {
 }
