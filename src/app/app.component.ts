6088f8a4e1d2684a502e2821d0a5c6cfc1b4c63e
Mevericks <reloved@gmail.com>
2019-01-24 11:21:09 +0200

    АНИМАЦИИ. trigger multi

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -24,11 +24,25 @@ import {animate, state, style, transition, trigger} from "@angular/animations";
       transition('start <=> end', animate(1500)),
       transition('start => active', animate(400)),
       transition('active => end', animate(400))
+    ]),
+    trigger('multi', [
+      state('start', style({
+        width: '150px',
+        height: '150px',
+        border: '5px solid black'
+      })),
+      state('end', style({
+        width: '170px',
+        height: '170px',
+        background: 'blue'
+      })),
+      transition('start <=> end', animate(500))
     ])
   ]
 })
 export class AppComponent {
   clickedDivState = 'start'
+  multiState = 'start'
 
   changeDivState(){
     this.clickedDivState = 'end'
