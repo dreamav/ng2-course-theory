96bb5d056ff6f4a652cecfec86aa860eb7d5a664
Mevericks <reloved@gmail.com>
2019-01-24 21:38:49 +0200

    АНИМАЦИИ. состояние void (для отсутств элементов в DOM)

--- src/app/app.animations.ts
+++ src/app/app.animations.ts
@@ -1,5 +1,12 @@
 import {animate, state, style, transition, trigger} from "@angular/animations";
 
 export const divTrigger = trigger('divTrigger',[
-
+  state('show', style({opacity:1})),
+  transition('void => show', [
+    style({opacity:0}),
+    animate(500, style({
+      opacity: 1
+    }))
+  ]),
+  transition('show => void', animate(500, style({opacity:0})))
 ]);
