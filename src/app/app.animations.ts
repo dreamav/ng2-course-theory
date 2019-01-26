7300e9a582cf507a8767ecdddabe9e862b3cee93
Mevericks <reloved@gmail.com>
2019-01-26 18:16:53 +0200

    АНИМАЦИИ. анимация при появлении блока с синим фоном

--- src/app/app.animations.ts
+++ src/app/app.animations.ts
@@ -2,17 +2,16 @@ import {animate, state, style, transition, trigger} from "@angular/animations";
 
 export const divTrigger = trigger('divTrigger',[
   transition(':enter', [
-    style({opacity:0}),
-    animate(500, style({
-      opacity: 1
-    }))
+    style({
+      width: '*',
+      height: '*'
+    }),
+    animate(2000, style({
+      width:'200px',
+      height:'200px',
+      backgroundColor: 'blue'
+    })),
+    animate(1000)
   ]),
   transition(':leave', animate(500, style({opacity:0})))
 ]);
-
-export const changeWidthTrigger = trigger('changeWidth',[
-  transition('*=>*',[
-    animate(1000,style({width:'10px'})),
-    animate(1000,style({width:'*'}))
-  ])
-])
